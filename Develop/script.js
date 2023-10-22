32cec836c5cc011b592dde0446766d6e5b1f6a45
Jen0234 <JennaLafable@gmail.com>
2023-10-22 18:39:51 -0400

    Update script.js
    add Password Characters
    - Number
    - LowerCase
     - UpperCase
    - Special Characters
    
    - Prompt to display a question about the length of the password
    8 is the minimal length and 20 is the max

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,12 @@
 // Assignment code here
+number = [0,1, 2, 3, 4, 5, 6, 7, 8 , 9];
+lowerCase = ['qwertyuiopasdfghjklzxcvbnm'];
+upperCase = ['QWERTYUIOPASDFGHJKLZXCVBNM'];
+specialChar= ['@#$%^&*()_+{}[]~`-=|\\:;"<>./?'];
+PasswordLengthMin = 8;
+PasswordLengthMax = 20;
 
+pass = [number + lowerCase + upperCase + specialChar];
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -8,10 +15,23 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
+  function generatePassword() {
+    var passwordLength = prompt("How many characters would you like your password to be?");
+    if (passwordLength < PasswordLengthMin || passwordLength > PasswordLengthMax) {
+      alert("Password must be between " + PasswordLengthMin + " and " + PasswordLengthMax + " characters");
+      return generatePassword();
+    }
+    var password = "";
+    for (var i = 0; i < passwordLength; i++) {
+      var randomNumber = Math.floor(Math.random() * pass.length);
+      password += pass[randomNumber][Math.floor(Math.random() * pass[randomNumber].length)];
+    }
+    return password;
+  }
   passwordText.value = password;
 
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
